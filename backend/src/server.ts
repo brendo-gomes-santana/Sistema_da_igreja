75215484822c83b123e841d8debfdb9202013b3e
Brendo Gomes Santana <brengogs2021@gmail.com>
2025-08-28 11:17:59 -0300

    ajustando o jwt no backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,10 +3,10 @@ import { fastifyCors } from '@fastify/cors';
 import { fastifySwagger } from '@fastify/swagger';
 import { validatorCompiler, serializerCompiler, jsonSchemaTransform } from 'fastify-type-provider-zod';
 import fastifySwaggerUi from '@fastify/swagger-ui';
-import fastifyJwt from '@fastify/jwt';
 
 
-import clientWhatsApp from './function/whatsApp';
+import auth from './plugin/auth';
+
 import { routes } from './routes';
 import { ZodTypeProvider } from 'fastify-type-provider-zod';
 
@@ -43,18 +43,9 @@ app.register(fastifySwagger, {
 app.register(fastifySwaggerUi, {
     routePrefix: '/docs'
 })
-app.register(fastifyJwt, {
-    secret: process.env.SECRET as string
-})
 
-app.decorate("authenticate", async (request: FastifyRequest, reply: FastifyReply) => {
-    try {
-        await request.jwtVerify();
-    } catch (err) {
-        reply.send(err);
-    }
-});
-app.register(routes)
+app.register(auth);
+app.register(routes);
 
 app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
     console.log('HTTP server runing!')
