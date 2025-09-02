d3daf941a85d6270547234100db74e6152da7bec
Brendo Gomes Santana <brengogs2021@gmail.com>
2025-09-02 15:22:19 -0300

    organizando o backend

--- backend/src/server/Musics/CreateMusicServer.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = CreateMusicServer;
-const prisma_1 = require("../../prisma");
-async function CreateMusicServer(body) {
-    return await prisma_1.prisma.musics.create({
-        data: body
-    });
-}
--- backend/src/server/Types/listTypesServer.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = ListTypesServer;
-const prisma_1 = require("../../prisma");
-async function ListTypesServer() {
-    return await prisma_1.prisma.types.findMany();
-}
